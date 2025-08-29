56bf05929a3400561cb245013cd72050db9d1cae
Pratik Kad <kadpratik22@gmail.com>
2025-08-29 22:56:49 +0530

    home / landing page

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import img from '../assets/img2.jpg';
+import { Button } from '../components/ui/button.jsx';
+import Car from '../components/Car.jsx';
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '../components/ui/dialog.jsx';
+import CarDetails from '../components/CarDetails.jsx';
+import Review from '../components/Review.jsx';
+import { testCar } from '../lib/cardata.js';
+import { Card, CardContent, CardHeader, CardTitle } from '../components/ui/card';
+
+const Home = () => {
+  const [open, setOpen] = useState(false);
+  const [selectedCar, setSelectedCar] = useState(null);
+
+  
+  const cars = [testCar];
+
+  return (
+    <div className='flex flex-col'>
+      {/* Hero Section */}
+      <div className="relative w-full h-88 ">
+        <img className="absolute inset-0 w-full h-full object-cover z-0" src={img} alt="" />
+        <div className="absolute z-10 w-[40%] top-22 left-4">
+          <h1 className="text-4xl font-bold text-white mb-4">Find Your Perfect Drive</h1>
+          <p className='text-zinc-100/80 leading-5 text-lg'>
+            Discover the easiest way to buy and sell cars online. Browse thousands of vehicles or list yours in minutes.
+          </p>
+          <div className='flex gap-4 my-6'>
+            <Button variant="secondary">Browse Cars</Button>
+            <Button variant="outline" className="text-zinc-100">Sell Your Car</Button>
+          </div>
+        </div>
+      </div>
+
+      {/* Car Listing */}
+      {/* <div className='p-4'>
+        <h1 className='text-2xl font-semibold text-zinc-950/80'>Listed Vehicles</h1>
+        <div className='flex flex-wrap gap-4 mt-4'>
+          {cars.map((car, idx) => (
+            <div 
+              key={idx}
+              onClick={() => { setSelectedCar(car); setOpen(true); }}
+              className="cursor-pointer"
+            >
+              <Car car={car} />
+            </div>
+          ))}
+        </div>
+      </div> */}
+
+      <Card className="mt-4 mx-2">
+        <CardHeader>
+          <CardTitle>Listed Vehicles</CardTitle>
+        </CardHeader>
+        
+        <CardContent className="flex gap-2">
+            {cars.map((car, idx) => (
+            <div 
+              key={idx}
+              onClick={() => { setSelectedCar(car); setOpen(true); }}
+              className="cursor-pointer"
+            >
+              <Car car={car} />
+            </div>
+          ))}
+        </CardContent>
+      </Card>
+
+      
+
+      {/* Car Details Dialog */}
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent className="min-w-3xl max-h-[88vh] overflow-y-auto scrollbar-hide">
+          <DialogHeader>
+            <DialogTitle>{selectedCar?.name}</DialogTitle>
+          </DialogHeader>
+            {selectedCar && <CarDetails car={selectedCar} />}
+        </DialogContent>
+      </Dialog>
+
+      <div className="bottom flex flex-col items-center py-4 justify-center">
+        <p className="font-bold text-2xl">What Our Customers Say</p>
+        <div className='flex gap-2 justify-center flex-wrap'>
+          <Review />
+          <Review />
+          <Review />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
