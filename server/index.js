b2a35f3b43ebfc031842c81672c1b61cb50e1b47
Pratik Kad <kadpratik22@gmail.com>
2025-08-30 18:20:51 +0530

    Update server entry

--- server/index.js
+++ server/index.js
@@ -1,11 +1,34 @@
 import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+import router from "./routes/User.route.js";
+import cors from "cors"
+dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+app.use(
+    cors({
+      origin: [process.env.BASE_URL, 'http://localhost:5173'],
+      credentials: true,
+      methods: ["GET", "POST", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+  );
+
+
+connectDB()
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Server running with import syntax 🚀");
 });
+
+app.use("/user", router);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
