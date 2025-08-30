63339a41facf6699dcd92e1ad251a838cf134498
Pratik Kad <kadpratik22@gmail.com>
2025-08-30 18:21:56 +0530

    Add middleware

---
+++ server/middleware/user.middleware.js
@@ -0,0 +1,47 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+const isLoggedIn = (req, res, next) => {
+  try {
+    const token = req.cookies?.token; // use cookies
+    // const token = req.headers.authorization?.split(" ")[1]; // via header
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Token not found!",
+      });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; 
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Token not verified!",
+      error: error.message,
+    });
+  }
+};
+
+const isAdmin = async (req, res, next)=>{
+    try {
+        if(req.user.role !== "admin"){
+            return res.status(400).json({
+                success:false,
+                message:"You are not authorized!"
+            })
+        }
+        next();
+    } catch (error) {
+        return res.status(400).json({
+            success:false,
+            message:"You are not authorized!",
+            error:error.message
+        })
+    }
+};
+
+export {isLoggedIn, isAdmin};
