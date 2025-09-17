c40892bc83c64e8a8a24538c15a710d81c280e64
Pratik Kad <kadpratik22@gmail.com>
2025-09-17 18:59:30 +0530

    car route added to index

--- server/index.js
+++ server/index.js
@@ -3,23 +3,23 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import router from "./routes/User.route.js";
-import cors from "cors"
+import cors from "cors";
+import carRouter from "./routes/Car.route.js";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(
-    cors({
-      origin: [process.env.BASE_URL, 'http://localhost:5173'],
-      credentials: true,
-      methods: ["GET", "POST", "DELETE", "OPTIONS"],
-      allowedHeaders: ["Content-Type", "Authorization"],
-    })
-  );
+  cors({
+    origin: [process.env.BASE_URL, "http://localhost:5173"],
+    credentials: true,
+  })
+);
 
+connectDB();
 
-connectDB()
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
@@ -29,6 +29,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/user", router);
+app.use("/cars", carRouter)
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
