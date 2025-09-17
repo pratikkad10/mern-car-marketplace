eb93b3057b5b4da7a67b3b727e63b8afb59cdc12
Pratik Kad <kadpratik22@gmail.com>
2025-09-17 19:14:22 +0530

    api service

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -5,18 +5,22 @@ const api = axios.create({
   withCredentials: true, // send cookies automatically
 });
 
+// Attach token automatically
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+// Auth APIs
 export const loginUser = (data) => api.post("/user/login", data);
 export const signupUser = (data) => api.post("/user/signup", data);
 export const getProfile = () => api.get("/user/profile");
-export async function logoutUser() {
-  const res = await fetch('http://localhost:8000/user/logout', {
-    method: 'POST',
-    credentials: 'include',
-  });
-  if (!res.ok) {
-    throw new Error('Logout failed');
-  }
-  return res.json();
-}
+export const logoutUser = () => api.post("/user/logout");
+
+export const CreateCar = (data) => api.post("/cars/car", data);
+export const getAllCars = () => api.get("/cars/");
 
 export default api;
