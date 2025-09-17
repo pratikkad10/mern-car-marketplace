948a14bdb8a99f6fe12c5994a23aae8c280df2e6
Pratik Kad <kadpratik22@gmail.com>
2025-09-17 19:08:18 +0530

    page footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,21 +1,21 @@
-import { CopyCheck, CopyrightIcon } from 'lucide-react'
+// Footer.jsx
+import { CopyrightIcon } from 'lucide-react'
 import React from 'react'
 
-const Footer = () => {
-    return (
-        <div className=''>
-            <div className=' flex items-center justify-between px-4 h-14 w-full bg-[#101826] z-10'>
-                <p className='text-zinc-300/80 text-sm flex gap-1  items-center'>
-                    <CopyrightIcon className='h-3 w-3 ' /> <span>2025 AutoMarket. All rights reserved.</span>
-                </p>
-                <div className='flex gap-4 text-zinc-300/80 text-sm'>
-                    <span>Privacy Policy</span>
-                    <span>Terms of Service</span>
-                    <span>Cookie Policy</span>
-                </div>
-            </div>
-        </div>
-    )
-}
+const Footer = () => (
+  <footer className="w-full -mb-10 bg-[#101826]">
+    <div className="flex items-center justify-between px-4 h-14">
+      <p className="text-zinc-300/80 text-sm flex gap-1 items-center">
+        <CopyrightIcon className="h-3 w-3" /> 
+        <span>2025 AutoMarket. All rights reserved.</span>
+      </p>
+      <div className="flex gap-4 text-zinc-300/80 text-sm">
+        <span className="cursor-pointer hover:text-white">Privacy Policy</span>
+        <span className="cursor-pointer hover:text-white">Terms of Service</span>
+        <span className="cursor-pointer hover:text-white">Cookie Policy</span>
+      </div>
+    </div>
+  </footer>
+);
 
-export default Footer
+export default Footer;
