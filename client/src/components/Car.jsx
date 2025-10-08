358066d365569d03f5c17d9e961bd7dc2fad36fd
pratik kad <148075940+pratikkad10@users.noreply.github.com>
2025-10-08 16:33:19 +0530

    car card updated

--- client/src/components/Car.jsx
+++ client/src/components/Car.jsx
@@ -5,19 +5,19 @@ import { Fuel, FuelIcon, Settings } from 'lucide-react'
 
 const Car = ({car}) => {
     return (
-        <div className='w-90 rounded-xl shadow hover:shadow-md overflow-hidden'>
-            <div>
-                <img src={car.images[0]} className='rounded-lg h-52 w-full' alt="" />
+        <div className='w-full h-full rounded-xl shadow hover:shadow-md overflow-hidden bg-card text-card-foreground flex flex-col'>
+            <div className='w-full'>
+                <img src={car.images[0]} className='h-52 w-full object-cover' alt="" />
             </div>
-            <h1 className='font-semibold text-lg text-zinc-950/80 px-4 pt-2' >{car.carName}</h1>
-            <h1 className='font-bold text-xl text-zinc-950/80 px-4 pt-1'>₹{car.price}</h1>
+            <h1 className='font-semibold text-lg px-4 pt-2 text-card-foreground'>{car.carName}</h1>
+            <h1 className='font-bold text-xl px-4 pt-1 text-foreground'>₹{car.price}</h1>
 
-            <div className='flex gap-4 mx-4 mt-4 font-extralight'>
+            <div className='flex gap-4 px-4 mt-4 font-extralight'>
                 <span className='flex gap-1 items-center'> <FuelIcon className='h-4 w-4' /> {car.fuelType}</span>
                 <span className='flex gap-1 items-center'> <Settings className='h-4 w-4'/> {car.transmission}</span>
             </div>
-            <div className='p-2'>
-                <Button className="w-full cursor-pointer" variant="default">View Details</Button>
+            <div className='p-4 mt-auto'>
+                <Button className="w-full cursor-pointer bg-primary text-primary-foreground hover:bg-primary/90" variant="default">View Details</Button>
             </div>
         </div>
     )
