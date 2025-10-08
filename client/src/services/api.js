b719c2916fc7b37748038a7074ae30a7d0a7ae91
pratik kad <148075940+pratikkad10@users.noreply.github.com>
2025-10-08 13:22:37 +0530

    api added

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -19,8 +19,17 @@ export const loginUser = (data) => api.post("/user/login", data);
 export const signupUser = (data) => api.post("/user/signup", data);
 export const getProfile = () => api.get("/user/profile");
 export const logoutUser = () => api.post("/user/logout");
+export const resetPassword = (data) => api.post("/user/reset-password", data);
 
 export const CreateCar = (data) => api.post("/cars/car", data);
 export const getAllCars = () => api.get("/cars/");
+export const deleteCar = (data)=> api.delete('/cars/car',{ data});
+
+export const createTestDrive = (data) => api.post("/cars/test-drive", data);
+export const getTestDrives = () => api.get("/cars/test-drive");
+export const getAllTestDrives = () => api.get("/cars/allTestDrive");
+export const createIntrestedUser = (data) => api.post("/cars/interestedUser", data);
+export const getIntrestedUser = () => api.get("/cars/interestedUser");
+
 
 export default api;
