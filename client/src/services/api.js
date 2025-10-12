0260937fe3e13fe85fc50d48e2513005624a77df
pratik kad <148075940+pratikkad10@users.noreply.github.com>
2025-10-12 22:27:06 +0530

    backend url added

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://car-marketplace-backend-6kpu.onrender.com",
+  baseURL: "https://carmarketplace-backend.vercel.app/",
   withCredentials: true, // send cookies automatically
 });
 
